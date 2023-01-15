2709e2ca83bba97fb368650d3e65419e639f0a04
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-01-15 18:37:53 +0300

    git@github.com:LinaAlekseeva/HW_Selenide_4.git

--- src/test/java/TestFormForRegistration.java
+++ src/test/java/TestFormForRegistration.java
@@ -1,35 +1,53 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
     public class TestFormForRegistration {
+        @BeforeAll
+        static void beforeAll() {
+            Configuration.browserSize = "1920x1080";
+            Configuration.baseUrl = "https://demoqa.com";
+        }
        @Test
       void studentRegistrationForm() {
-       open("https://demoqa.com/automation-practice-form");
-       $(".main-header").shouldHave(text("Practice Form"));
-       $("#firstName ").setValue("Pavel");
-       $("#lastName ").setValue("Lepet");
-       $("#userEmail ").setValue("pavellepet@gmail.com");
-       //gender
-       $("#gender-radio-1").shouldHave(text("Male")).click();
-       $("#userNumber ").setValue("8455345245");
-       //Date of Birth
-       $("#dateOfBirthInput ").click();
-       $(".react-datepicker__header").click();
-       $(byText("2023")).hover();
-       $(byText("1995")).click();
+        String name = "Pavel";
+        String lastName = "Lepet";
+        String email = "pavellepet@gmail.co";
+        String phoneNumber = "8455345245";
+        String subject = "English";
+        String currentAddress = "2600 North Military Trail West Palm Beach FL 33409-0000 USA";
 
-       $("#subjectsContainer ").setValue("History");
+       open("/automation-practice-form");
+       $(".main-header").shouldHave(text("Practice Form"));
+       $("#firstName ").setValue(name);
+       $("#lastName ").setValue(lastName);
+       $("#userEmail ").setValue(email);
+       //gender
+       $("#gender-radio-1").parent().click();
+       $("#userNumber ").setValue(phoneNumber);
+       //Date of Birth
+       $("#dateOfBirthInput").click();
+       $(".react-datepicker__month-select").selectOption("November");
+       $(".react-datepicker__year-select").selectOption("1995");
+       $(".react-datepicker__day--017").click();
+       $("#subjectsInput ").setValue(subject).pressTab().click();
        //Hobbies
+       $("#hobbiesWrapper").$(byText("Sports")).click();
        //Picture
-       $("#currentAddress").setValue("2600 North Military Trail West Palm Beach FL 33409-0000 USA");
+       $("#uploadPicture").uploadFromClasspath("img\\1.pmg");
+       $("#currentAddress").setValue(currentAddress);
        //State and City
-       $("#submit").shouldHave(text("Submit")).click();
-       sleep(500);
-       $("#output").shouldBe(visible);
-       $("#output #email").shouldHave(text("pavellepet@gmail.com"));
+       $("#state").click();
+       $("#stateCity-wrapper").$(byText("Haryana")).click();
+       $ ("#city").click();
+       $("#stateCity-wrapper").$(byText("Karnal")).click();
+       $("#submit").shouldHave(text("Submit")).pressEnter();
+       $(".modal-content").should(appear);
+       $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
     }
 }
