592f1c36333461177b4aef0598825b4b1892fe79
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-01-17 13:10:31 +0300

    Registration with PageObjects

---
+++ src/test/java/tests/TestFormForRegistrationWithPageObjects.java
@@ -0,0 +1,27 @@
+package tests;
+import org.junit.jupiter.api.Test;
+
+
+public class TestFormForRegistrationWithPageObjects extends TestBase {
+    @Test
+    void studentRegistrationForm() {
+        registrationPage
+                .openPage()
+                .setFirstName("Pavel")
+                .setLastName("Lepet")
+                .setEmail("pavellepet@gmail.com")
+                .setGender()
+                .setUserNumber("8455345245")
+                .setBirthDate("6", "Jul", "1995")
+                .setAddress("2600 North Military Trail West Palm Beach FL 33409-0000 USA")
+                .setHobbies("Sports")
+                .setPicture("img\\1.pmg")
+                .setState("Haryana", "Karnal")
+                .setSubmit("Submit");
+
+        registrationPage.verifyResultsModalAppears()
+                .verifyResults(" Student Name", "Pavel Lepet")
+                .verifyResults(" Student Email", "pavellepet@gmail.com")
+                .verifyResults(" Mobile", "8455345245");
+    }
+}
