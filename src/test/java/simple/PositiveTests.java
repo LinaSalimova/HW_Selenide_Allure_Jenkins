333436b1cda4405a58e6da43dc7598b276970b3e
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-03-05 16:06:36 +0300

    +Jenkins

--- src/test/java/simple/PositiveTests.java
+++ src/test/java/simple/PositiveTests.java
@@ -3,7 +3,7 @@ package simple;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PositiveTests {
     @Tag("simple")
--- src/test/java/simple/SkippedTests.java
+++ src/test/java/simple/SkippedTests.java
@@ -3,7 +3,7 @@ package simple;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SkippedTests {
     @Test
