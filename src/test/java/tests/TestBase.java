563bca21bf3d2e15d5cc17fcc6e2de4493cf6fa0
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-03-08 14:43:51 +0300

    +added remote test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 public class TestBase {
@@ -27,6 +28,13 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 }
