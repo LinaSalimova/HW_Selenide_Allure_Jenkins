42959205d0c9a9227e374b1ec2512a5edde42352
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-03-27 16:16:06 +0300

    +

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -34,10 +34,10 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
