c2e3d990bce891ffce77e182a4f791d1dde378dd
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-03-27 12:35:54 +0300

    +

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -34,7 +34,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = System.getProperty("browserSize", "1980x1020");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
