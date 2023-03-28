620efdbe08cec94be6983ea0d65a0c852e9133a1
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-03-28 17:17:04 +0300

    +

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -38,7 +38,7 @@ public class TestBase {
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
